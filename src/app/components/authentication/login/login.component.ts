9a8f992cee61c69048b6f96eef50e28e62c52550
Rohit kadam <rohitkadam1407@gmail.com>
2019-03-18 13:42:36 +0530

    login and registration page

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -13,21 +13,34 @@ export class LoginComponent implements OnInit {
 
   constructor( private userCollection: UsersService) 
   {
-      this.userObj = new User("rohit","kadam","rk@gmail.com");
-      userCollection.insert(this.userObj).then( ()=> {
-        userCollection.findAll().then( (value)=> {
-          console.log("List of user are ");
-          console.log(value);
-        })
-        .catch((err)=>{
-          console.log("Error: "+err);
-        });
+      // this.userObj = new User("rohit","kadam","rk@gmail.com");
+      // userCollection.insert(this.userObj).then( ()=> {
+      //   userCollection.findAll().then( (value)=> {
+      //     console.log("List of user are ");
+      //     console.log(value);
+      //   })
+      //   .catch((err)=>{
+      //     console.log("Error: "+err);
+      //   });
 
-      })
+      // })
+
+
     
   }
+
 
   ngOnInit() {
   }
 
+  username: string;
+  password: string;
+
+  login() : void {
+    if(this.username == 'admin' && this.password == 'admin'){
+      alert("valid username");
+    }else {
+      alert("Invalid credentials");
+    }
+  }
 }
--- src/app/components/authentication/registration/registration.component.ts
+++ src/app/components/authentication/registration/registration.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { Profile } from 'selenium-webdriver/firefox';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +9,77 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  public registrationForm: FormGroup;
+  public uploadControl: FormGroup;
 
-  ngOnInit() {
+  public imagePath;
+  imgURL: any;
+  public message: string;
+  public profilePic;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() 
+  {
+    this.registrationForm = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(6)]]
+    });
+
+    this.uploadControl = this._formBuilder.group(
+      {
+        addPhoto : [null,Validators.required] 
+      }
+    )
+
   }
 
+  preview(files) {
+    if (files.length === 0)
+      return;
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.message = "Only images are supported.";
+      return;
+    }
+    var reader = new FileReader();
+    this.imagePath = files;
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.imgURL = reader.result;
+    }
+  }
+
+  public onSubmit()
+  {
+
+  }
+
+  // Image Reader
+  public onFileInput(event: any)
+  {
+    for( let i = 0 ; i < event.target.files.length; i++)
+    {
+        // this.photoInfos.push(new PhotoInfo(event.target.files[i].path));
+        this.readTheImagedata(event.target.files[i]); 
+    }
+  }
+
+  public readTheImagedata(file)
+  {
+    let reader = new FileReader();
+    reader.onload = (event: any) => 
+    {  
+      // this.images.push(event.target.result);
+      this.profilePic = event.target.result;
+    }
+    reader.readAsDataURL(file);
+  }
+
+  public addUser()
+  {
+
+  }
 }
